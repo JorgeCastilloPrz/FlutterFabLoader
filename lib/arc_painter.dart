67195a80805cd5d16f1d52e70501cf8e9ff596da
jorgecastilloprz <jorge.castillo.prz@gmail.com>
2019-05-29 15:16:36 +0200

    remove non needed nesting

--- lib/arc_painter.dart
+++ lib/arc_painter.dart
@@ -1,31 +1,61 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 class ArcPainter extends CustomPainter {
-  final double strokeWidth;
-  final double progress;
-  final Color backgroundColor;
-  final Color color;
+  ArcPainter({
+    this.backgroundColor,
+    this.valueColor,
+    this.headValue,
+    this.tailValue,
+    this.stepValue,
+    this.rotationValue,
+    this.strokeWidth,
+  }) : arcStart = _startAngle + tailValue * 3 / 2 * pi + rotationValue * pi * 1.7 - stepValue * 0.8 * pi,
+        arcSweep = max(headValue * 3 / 2 * pi - tailValue * 3 / 2 * pi, _epsilon);
 
-  ArcPainter(
-      {this.strokeWidth, this.progress, this.backgroundColor, this.color});
+  final Color backgroundColor;
+  final Color valueColor;
+  final double headValue;
+  final double tailValue;
+  final int stepValue;
+  final double rotationValue;
+  final double strokeWidth;
+  final double arcStart;
+  final double arcSweep;
+
+  static const double _twoPi = pi * 2.0;
+  static const double _epsilon = .001;
+  // Canvas.drawArc(r, 0, 2*PI) doesn't draw anything, so just get close.
+  static const double _sweep = _twoPi - _epsilon;
+  static const double _startAngle = -pi / 2.0;
 
   @override
   void paint(Canvas canvas, Size size) {
-    var paint = new Paint();
-    paint
-      ..color = backgroundColor
+    final Paint paint = Paint()
+      ..color = valueColor
       ..strokeWidth = strokeWidth
       ..style = PaintingStyle.stroke;
+    if (backgroundColor != null) {
+      final Paint backgroundPaint = Paint()
+        ..color = backgroundColor
+        ..strokeWidth = strokeWidth
+        ..style = PaintingStyle.stroke;
+      canvas.drawArc(Offset.zero & size, 0, _sweep, false, backgroundPaint);
+    }
 
-    canvas.drawCircle(
-        new Offset(size.width / 2, size.height / 2), size.width / 2, paint);
+    paint.strokeCap = StrokeCap.square;
 
-    canvas.drawArc(new Offset(0.0, 0.0) & new Size(size.width, size.width),
-        -90.0 * 0.0174533, progress * 0.0174533, false, paint..color = color);
+    canvas.drawArc(Offset.zero & size, arcStart, arcSweep, false, paint);
   }
 
   @override
-  bool shouldRepaint(ArcPainter oldDelegate) {
-    return oldDelegate.progress != progress;
+  bool shouldRepaint(ArcPainter oldPainter) {
+    return oldPainter.backgroundColor != backgroundColor
+        || oldPainter.valueColor != valueColor
+        || oldPainter.headValue != headValue
+        || oldPainter.tailValue != tailValue
+        || oldPainter.stepValue != stepValue
+        || oldPainter.rotationValue != rotationValue
+        || oldPainter.strokeWidth != strokeWidth;
   }
 }
--- lib/fab_loader.dart
+++ lib/fab_loader.dart
@@ -8,6 +8,7 @@ import 'arc_painter.dart';
 /// Draws a loading arc around the child view, supposed to be a
 /// FloatingActionButton.
 class FabLoader extends StatefulWidget {
+
   final Color color;
   final Color backgroundColor;
   final double strokeWidth;
@@ -46,7 +47,25 @@ const BoxConstraints _miniSizeConstraints = BoxConstraints.tightFor(
   height: 40.0,
 );
 
-const ROTATE_ANIMATION_DURATION = 2000;
+// Arc head will be lead by this Tween curve.
+final Animatable<double> _kStrokeHeadTween = CurveTween(
+  curve: const Interval(0.0, 0.5, curve: Curves.fastOutSlowIn),
+).chain(CurveTween(
+  curve: const SawTooth(5),
+));
+
+// Arc tail will be lead by this Tween curve.
+final Animatable<double> _kStrokeTailTween = CurveTween(
+  curve: const Interval(0.5, 1.0, curve: Curves.fastOutSlowIn),
+).chain(CurveTween(
+  curve: const SawTooth(5),
+));
+
+// The current progress step will be lead by this stepped Tween.
+final Animatable<int> _kStepTween = StepTween(begin: 0, end: 5);
+
+// The rotation for the arc as a whole will be lead by this Tween curve.
+final Animatable<double> _kRotationTween = CurveTween(curve: const SawTooth(5));
 
 class _FabLoadingWidget extends State<FabLoader>
     with SingleTickerProviderStateMixin {
@@ -54,8 +73,7 @@ class _FabLoadingWidget extends State<FabLoader>
   final double strokeWidth;
   final BoxConstraints sizeConstraints;
 
-  AnimationController controller;
-  Animation animation;
+  AnimationController _controller;
 
   _FabLoadingWidget(
       {@required this.sizeConstraints,
@@ -65,36 +83,63 @@ class _FabLoadingWidget extends State<FabLoader>
   @override
   void initState() {
     super.initState();
-    controller = new AnimationController(
-        vsync: this,
-        duration: const Duration(milliseconds: ROTATE_ANIMATION_DURATION))
-      ..repeat();
+    _controller = AnimationController(
+      duration: const Duration(seconds: 5),
+      vsync: this,
+    );
+    _controller.repeat(); // we want it to repeat over and over (indeterminate)
+  }
 
-    var curve = new CurvedAnimation(parent: controller, curve: Curves.linear);
-    animation = Tween(begin: 0.0, end: 360.0).animate(curve);
+  @override
+  void didUpdateWidget(FabLoader oldWidget) {
+    super.didUpdateWidget(oldWidget);
+    // We want to start animation again if the widget is updated.
+    if (!_controller.isAnimating) {
+      _controller.repeat();
+    }
+  }
 
-    controller.addListener(() {
-      setState(() {});
-    });
+  @override
+  void dispose() {
+    _controller.dispose(); // avoid leaks
+    super.dispose();
+  }
+
+  Widget _buildIndicator(BuildContext context, double headValue,
+      double tailValue, int stepValue, double rotationValue) {
+    var size = sizeConstraints;
+    return new CustomPaint(
+      child: child,
+      foregroundPainter: new ArcPainter(
+          strokeWidth: strokeWidth,
+          backgroundColor: widget.backgroundColor,
+          valueColor: widget.color,
+          headValue: headValue,
+          tailValue: tailValue,
+          stepValue: stepValue,
+          rotationValue: rotationValue),
+      size: new Size(
+          size.maxWidth + strokeWidth * 2, size.maxHeight + strokeWidth * 2),
+    );
+  }
+
+  Widget _buildAnimation() {
+    return AnimatedBuilder(
+      animation: _controller,
+      builder: (BuildContext context, Widget child) {
+        return _buildIndicator(
+          context,
+          _kStrokeHeadTween.evaluate(_controller),
+          _kStrokeTailTween.evaluate(_controller),
+          _kStepTween.evaluate(_controller),
+          _kRotationTween.evaluate(_controller),
+        );
+      },
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    var size = sizeConstraints;
-    return new Stack(
-      alignment: Alignment.center,
-      children: <Widget>[
-        child,
-        new CustomPaint(
-          painter: new ArcPainter(
-              strokeWidth: strokeWidth,
-              progress: animation.value,
-              backgroundColor: widget.backgroundColor,
-              color: widget.color),
-          size: new Size(
-              size.maxWidth + strokeWidth, size.maxHeight + strokeWidth),
-        ),
-      ],
-    );
+    return _buildAnimation();
   }
 }
